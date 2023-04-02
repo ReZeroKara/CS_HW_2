63e4d04eb2c346fcd3f507e661d000797f8e6e8b
ReZero <ghostofpast@icloud.com>
2023-04-02 20:51:26 +0300

    add task002

---
+++ task002_ThrdNum/Program.cs
@@ -0,0 +1,60 @@
+﻿//Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Main();
+
+void Main()
+{
+    bool isWorking = true;
+
+    while(isWorking)
+    {
+        Console.Write("Input task: ");
+
+        string task = Console.ReadLine();
+
+        switch(task)
+        {
+            case "exit": isWorking = false; break;
+            case "t1": Task1(); break;
+            default:
+            break;
+        }
+    }
+}
+
+void Task1()
+{
+    int first = fint("a");
+    Console.WriteLine(first);
+}
+
+int fint(string num)
+{
+    int charm = Readint("n");
+
+    if(charm > 100)
+    {
+      charm = charm % 10;
+      return charm;  
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет!");
+        return charm;
+    }
+}
+
+int Readint(string argument)
+{
+    int number;
+
+    Console.Write($"Input {argument}:");
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It's not a number!");
+    }
+    return number;
+}
