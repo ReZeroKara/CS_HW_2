e239db92cb8a6255e720441323d1e159c3047588
ReZero <ghostofpast@icloud.com>
2023-04-02 21:07:01 +0300

    add Weekend task

---
+++ task003_Weekend/Program.cs
@@ -0,0 +1,31 @@
+﻿//Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int[] array = {0, 1, 2, 3, 4, 5, 6, 7};
+
+Console.Write("Введите номер дня недели: ");
+int find = int.Parse(Console.ReadLine());
+
+int n = array.Length;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        if(find == array[6])
+        {
+            Console.WriteLine("Суббота, это выходной!");
+        }
+        else if(find == array[7])
+        {
+            Console.WriteLine("Воскресенье, это выходной!");
+        }
+        else
+        {
+            Console.WriteLine("Это не выходной!");
+        }
+        break;
+    }
+    index++;
+}
